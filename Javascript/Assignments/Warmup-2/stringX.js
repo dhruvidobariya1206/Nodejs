1f6f8496e4e0532833ecfc767b9faddfa5837d79
Dhruvi <dhruvi.dobariya110826@marwadiuniversity.ac.in>
2022-07-05 22:59:51 +0530

    stringX

--- Javascript/Assignments/Warmup-2/stringX.js
+++ Javascript/Assignments/Warmup-2/stringX.js
@@ -1,35 +1,17 @@
 function stringX(str){
-    // let i;
-    // for(i=0; i<=str.length; i++) {
-    //     console.log(i);
-    //     if(str.charAt(i)=='x') {
-    //         str.replace(str.charAt(i),'');
-    //         console.log(str);
-    //     }
-    // }
-    // return str;
-    if (str.length<=2) {
-        return str;
+    let start='',end='';
+    if(str.charAt(0)=='x') {
+        start = str.charAt(0);
     }
-    let i;
-    
-        let start= str.charAt(0) ;
-    let end= str.charAt(str.length-1);
-    for(i=1; i<=str.length; i++) {
+    if(str.charAt(str.length-1)=='x') {
+        end = str.charAt(str.length-1);
+    }
+    for(i=1; i<=str.length+1; i++) {
         str = str.replace("x", "");
     }
-    if(str.charAt(0)=='x' && str.charAt(str.length-1)=='x') {
         return start + str + end;
-    }
-    else if(str.charAt(0)!='x' && str.charAt(str.length-1)=='x') {
-        return str+end;
-    }
-    else {
-        return start+str;
-    }
-
     
 }
 
-let answer = stringX('cxoxdxex');
+let answer = stringX('xxHxix');
   console.log(answer);
