c3b9919d9e295bcf7073c14ecbc1ec22ebfd8f81
Dhruvi <dhruvi.dobariya110826@marwadiuniversity.ac.in>
2022-07-08 21:31:37 +0530

    front111.js

--- Javascript/Assignments/Array-1/front11.js
+++ Javascript/Assignments/Array-1/front11.js
@@ -1,19 +1,13 @@
 function front11(a, b){
-    let a1 = [a[0]], b1=[b[0]];
-    let ans = [{}];
-    if(a.length>0 && b.length>0) {
-        return a1.concat(b1);
-    }
-    else if(a.length==0) {
-        return b1;
-    }
-    else if(b.length==0) {
-        return a1;
-    }
-    else {
-        return ans;
+    if (a.length == 0 && b.length == 0)
+      return []
+    else if (a.length !=0 && b.length == 0)
+      return [a[0]]
+    else if (a.length == 0 && b.length != 0)
+      return [b[0]]
+    else
+      return [a[0],b[0]]
   }
-}
 
 let answer = front11([], []);
 console.log(answer);
