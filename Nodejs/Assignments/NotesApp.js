be39159edc38af329b2ba3f75f0e5fdbb3625365
Dhruvi <dhruvi.dobariya110826@marwadiuniversity.ac.in>
2022-07-13 14:38:18 +0530

    index

--- Nodejs/Assignments/NotesApp.js
+++ Nodejs/Assignments/NotesApp.js
@@ -2,24 +2,23 @@ const fs=require('fs');
 const path = require('path');
 const readline = require("readline-sync");
 
-// console.log(__filename);
+
 const folPath = path.join(__dirname,'Files');
-// const filePath = '${folPath}\Current-File.txt';
+
 
 console.log("List");
-console.log("1. New\n2. Write\n3. Update\n4. Rename\n5. Delete\n6. View All");
+console.log("1. New\n2. Write\n3. Update\n4. Rename\n5. Delete\n6. Read file\n7. View All");
 
-// const input = process.argv;
-console.log("Enter Your Choice")
-let choice = Number(readline.question());
-// console.log(a);
+
+let choice = Number(readline.question("Enter Your Choice : "));
+
 
 if(choice==1) {
-    console.log("Enter file name to be created")
-    const fileName = String(readline.question());
-    // const filePath = `${folPath}\${fileName}`;
-    // fs.writeFileSync(filePath,'test');
-    fs.open(fileName,'w+',function (err,f) {
+    
+    const fileName = String(readline.question("Enter file name to be created : "));
+    const file = `${folPath}/${fileName}`;
+    
+    fs.open(file,'w+',function (err,f) {
         if (err)
             console.log(err);
         if(f)
@@ -27,28 +26,64 @@ if(choice==1) {
     });
 }
 else if(choice==2) {
-    console.log("Enter file name to be written")
-    const fileName = String(readline.question());
-    console.log("Enter text")
-    const text = String(readline.question());
-    fs.write(fileName,text,
-        {
-            encoding: "utf8",
-            flag: "w",
-            mode: 0o666
-        },
-        function (err,f) {
-            if (err)
-                console.log(err);
-            if(f)
-                console.log("File Written");
-        })
+    
+    const fileName = String(readline.question("Enter file name to be written : "));
+    const file = `${folPath}/${fileName}`;
+    const text = String(readline.question("Enter text : "));
+    fs.writeFile(file, text, err => {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            console.log("File written");
+        }
+    })
 }
 else if(choice==3) {
-
+    const fileName = String(readline.question("Enter file name to be written : "));
+    const content = '\n'+String(readline.question("Enter text : "));
+    const file = `${folPath}/${fileName}`;
+    fs.appendFile(file, content, err => {
+        if (err) {
+          console.error(err);
+        }
+        else {
+            console.log("File appended");
+        }
+      });
 }
 else if(choice==4) {
+    const fileName = String(readline.question("Enter file name to be renamed : "));
+    const newName = String(readline.question("Enter new file : "));
+    const file = `${folPath}/${fileName}`;
+    const newfile = `${folPath}/${newName}`;
 
+    fs.rename(file, newfile,(err)=>{
+            if(err){
+              console.log('Error renaming file...');
+              return;
+            }
+            else console.log("File is renamed...");
+          });
+
+}
+else if(choice==5) {
+    const fileName = String(readline.question("Enter file name to be written : "));
+    const file = `${folPath}/${fileName}`;
+    fs.unlinkSync(file);
+}
+else if(choice == 7) {
+    fs.readdir(folPath, (err,files)=>{
+        if(err){
+          console.log('Error...');
+          return;
+        }
+        else{
+          files.forEach((fileName)=>{
+            console.log(fileName);
+          });
+        }
+      });
 }
 else {
     console.log("Invalid Choice");
