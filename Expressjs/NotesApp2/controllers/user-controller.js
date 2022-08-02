f00a2f3d3bcbfea741f5e892725b85e22f5a7d51
Dhruvi <dhruvi.dobariya110826@marwadiuniversity.ac.in>
2022-08-02 18:52:43 +0530

    index.js

--- Expressjs/NotesApp2/controllers/user-controller.js
+++ Expressjs/NotesApp2/controllers/user-controller.js
@@ -8,30 +8,12 @@ class UserController {
         this.mongoose = require('mongoose')
     }
 
-    async verifyUser(req,resp) {
-        //fetch credentials from request object
-        let userEmail = req.body.email;
-        let userPassword = req.body.password;
-
-        //pass credentials inside service function
-        let serviceResponse =await this.userService.checkUserCredentials(userEmail,userPassword);
-        console.log("ServiceResponse", serviceResponse)
-        //send proper response to user
-        let resStatus = 200
-        if(serviceResponse.status != true){
-            resStatus = 401
-        }
-        resp.status(resStatus).send(serviceResponse)
-    }
-
     async createUser (req,resp) {
         try {
             let userData = {
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password,
-                mobile_number: req.body.mobile_number,
-                age: req.body.age,
             }
 
             console.log("userData", userData)
@@ -52,151 +34,151 @@ class UserController {
         }
     }
 
-    async findUser(req,resp) {
-        try {
-            let email = req.params.email;
-            let findUser = await this.userService.findUser(email)
-            if(findUser.status){
-                resp.status(200).send({
-                    status:true,
-                    Data: findUser.data
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in finding user'
-                })
-            }
-        } catch (error) {
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while finding user'
-            })
-        }
-    }
+//     async findUser(req,resp) {
+//         try {
+//             let email = req.params.email;
+//             let findUser = await this.userService.findUser(email)
+//             if(findUser.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     Data: findUser.data
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in finding user'
+//                 })
+//             }
+//         } catch (error) {
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while finding user'
+//             })
+//         }
+//     }
 
-    async getUser(req,resp){
-        try{
-            let allUser = await this.userService.getAllUser()
-            if(allUser.status){
-                resp.status(200).send({
-                    status:true,
-                    Data: allUser.data
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in finding all user'
-                })
-            }
-        } catch(error){
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while finding user'
-            })
-        }
-    }
+//     async getUser(req,resp){
+//         try{
+//             let allUser = await this.userService.getAllUser()
+//             if(allUser.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     Data: allUser.data
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in finding all user'
+//                 })
+//             }
+//         } catch(error){
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while finding user'
+//             })
+//         }
+//     }
 
-    async deleteUser(req,resp){
-        try{
-            let userEmail = req.body.email
-            let deletedUser = await this.userService.deleteUser(userEmail)
+//     async deleteUser(req,resp){
+//         try{
+//             let userEmail = req.body.email
+//             let deletedUser = await this.userService.deleteUser(userEmail)
 
-            if(deletedUser.status){
-                resp.status(200).send({
-                    status:true,
-                    Data: deletedUser.data
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in deleting user'
-                })
-            }
-        } catch(error) {
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while deleting user'
-            })
-        }
-    }
+//             if(deletedUser.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     Data: deletedUser.data
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in deleting user'
+//                 })
+//             }
+//         } catch(error) {
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while deleting user'
+//             })
+//         }
+//     }
 
-    async updateUser(req,resp){
-        try{
-            let userEmail = req.body.email 
-            let updatedUser = await this.userService.updateUser(userEmail)
+//     async updateUser(req,resp){
+//         try{
+//             let userEmail = req.body.email 
+//             let updatedUser = await this.userService.updateUser(userEmail)
 
-            if(updatedUser.status){
-                resp.status(200).send({
-                    status:true,
-                    Data: updatedUser.data
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in deleting user'
-                })
-            }
-        } catch(error) {
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while deleting user'
-            })
-        }
-    }
+//             if(updatedUser.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     Data: updatedUser.data
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in deleting user'
+//                 })
+//             }
+//         } catch(error) {
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while deleting user'
+//             })
+//         }
+//     }
 
-    async insertUserData(req,resp) {
-        try {
-            let userData = {
-                name: this.mongoose.Types.ObjectId(req.body.user_id),
-                id_card: req.body.id_card,
-                roll_number: req.body.roll_number
-            }
+//     async insertUserData(req,resp) {
+//         try {
+//             let userData = {
+//                 name: this.mongoose.Types.ObjectId(req.body.user_id),
+//                 id_card: req.body.id_card,
+//                 roll_number: req.body.roll_number
+//             }
 
-            console.log("userData", userData)
-            let queryResponse = await this.userService.insertUserData(userData);
-            if(queryResponse.status){
-                resp.status(200).send({
-                    status:true,
-                    message: "User inserted"
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in inserting user'
-                })
-            }
-        } catch(err) {
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while inserting user data'
-            })
-        }
-    }
+//             console.log("userData", userData)
+//             let queryResponse = await this.userService.insertUserData(userData);
+//             if(queryResponse.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     message: "User inserted"
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in inserting user'
+//                 })
+//             }
+//         } catch(err) {
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while inserting user data'
+//             })
+//         }
+//     }
 
-    async collectUserData(req,resp) {
-        try {
-            let userId = req.params.id_card
-            let queryResponse = await this.userService.collectUserDataOfUser(userId)
+//     async collectUserData(req,resp) {
+//         try {
+//             let userId = req.params.id_card
+//             let queryResponse = await this.userService.collectUserDataOfUser(userId)
 
-            if(queryResponse.status){
-                resp.status(200).send({
-                    status:true,
-                    message: "User data collected"
-                })
-            } else {
-                resp.status(400).send({
-                    status: false,
-                    message: 'Error in collecting all data of user'
-                })
-            }
-        } catch(err) {
-            resp.status(400).send({
-                status: false,
-                message: 'Error in controller while collecting all data of user'
-            })
-        }
-    }
+//             if(queryResponse.status){
+//                 resp.status(200).send({
+//                     status:true,
+//                     message: "User data collected"
+//                 })
+//             } else {
+//                 resp.status(400).send({
+//                     status: false,
+//                     message: 'Error in collecting all data of user'
+//                 })
+//             }
+//         } catch(err) {
+//             resp.status(400).send({
+//                 status: false,
+//                 message: 'Error in controller while collecting all data of user'
+//             })
+//         }
+//     }
 }
 
 module.exports = new UserController();
--- Expressjs/NotesApp2/services/user-service.js
+++ Expressjs/NotesApp2/services/user-service.js
@@ -5,29 +5,29 @@ class UserService {
         this.userDataModel = require("../models/user-data")
     }
     
-    checkUserCredentials(email, password) {
-        if(email !== this.userConfig.email){
-            return {
-                status: false,
-                message: 'Email id is not verified'
-            };
-        }else if(password !== this.userConfig.password) {
-            return {
-                status: false,
-                message: 'Password id is not verified'
-            };
-        }else if(email === this.userConfig.email && password === this.userConfig.password) {
-            return {
-                status: true,
-                message: 'User verified'
-            };
-        }else {
-            return {
-                status: false,
-                message: 'Invalid Credentials'
-            };
-        }
-    }
+    // checkUserCredentials(email, password) {
+    //     if(email !== this.userConfig.email){
+    //         return {
+    //             status: false,
+    //             message: 'Email id is not verified'
+    //         };
+    //     }else if(password !== this.userConfig.password) {
+    //         return {
+    //             status: false,
+    //             message: 'Password id is not verified'
+    //         };
+    //     }else if(email === this.userConfig.email && password === this.userConfig.password) {
+    //         return {
+    //             status: true,
+    //             message: 'User verified'
+    //         };
+    //     }else {
+    //         return {
+    //             status: false,
+    //             message: 'Invalid Credentials'
+    //         };
+    //     }
+    // }
 
     async createUser(userData){
         try {
@@ -53,147 +53,147 @@ class UserService {
         }
     }
 
-    async findUser(userEmail){
-        try{
-            //findOne function: returns single document or return null if user dont exist returns in mongodb object
+    // async findUser(userEmail){
+    //     try{
+    //         //findOne function: returns single document or return null if user dont exist returns in mongodb object
 
-            let user = await this.userModel.findOne({email: userEmail}).lean()
-            user['dob'] = new Date()
-            // find function : return array of object(multiple document) or empty array returns in mongodb object
+    //         let user = await this.userModel.findOne({email: userEmail}).lean()
+    //         user['dob'] = new Date()
+    //         // find function : return array of object(multiple document) or empty array returns in mongodb object
 
-            //.lean() : converts mongodb object document to json format
+    //         //.lean() : converts mongodb object document to json format
 
-            return {
-                status:true,
-                data: user
-            }
-        }catch(error){
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while finding user'
-            }
-        }
-    }
+    //         return {
+    //             status:true,
+    //             data: user
+    //         }
+    //     }catch(error){
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while finding user'
+    //         }
+    //     }
+    // }
 
-    async getAllUser(){
-        try{
-            let allUsers = await this.userModel.find().lean()
-            return{
-                status:true,
-                Data: allUsers
-            }
-        }catch(error){
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while finding all user'
-            }
-        }
-    }
+    // async getAllUser(){
+    //     try{
+    //         let allUsers = await this.userModel.find().lean()
+    //         return{
+    //             status:true,
+    //             Data: allUsers
+    //         }
+    //     }catch(error){
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while finding all user'
+    //         }
+    //     }
+    // }
 
-    async deleteUser(userEmail) {
-        try{
-            //findOneAndDelete: delete single document
-            let deletedUser =  await this.userModel.findOneAndDelete({email: userEmail})
-            // deleteMany: delete multiple documents
+    // async deleteUser(userEmail) {
+    //     try{
+    //         //findOneAndDelete: delete single document
+    //         let deletedUser =  await this.userModel.findOneAndDelete({email: userEmail})
+    //         // deleteMany: delete multiple documents
 
-            return{
-                status:true,
-                Data: deletedUser
-            }
-        } catch(error) {
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while deleting user'
-            }
-        }
-    }
+    //         return{
+    //             status:true,
+    //             Data: deletedUser
+    //         }
+    //     } catch(error) {
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while deleting user'
+    //         }
+    //     }
+    // }
 
-    async updateUser(userData){
-        try{
-            //findOneandUpdate: return old data of user {options->{new:true}}, parameters: 1.query(match)
-            // 2. data/fields to update
-            let updatedUser = await this.userModel.findOneAndUpdate({email: userData.email},{
-                $set: {
-                    name: userData.name,
-                    mobile_number: userData.mobile_number,
-                    age: userData.age
-                }
-            },{new: true})
-             //updateOne: update only one document,similar to findOneandUpdate
+    // async updateUser(userData){
+    //     try{
+    //         //findOneandUpdate: return old data of user {options->{new:true}}, parameters: 1.query(match)
+    //         // 2. data/fields to update
+    //         let updatedUser = await this.userModel.findOneAndUpdate({email: userData.email},{
+    //             $set: {
+    //                 name: userData.name,
+    //                 mobile_number: userData.mobile_number,
+    //                 age: userData.age
+    //             }
+    //         },{new: true})
+    //          //updateOne: update only one document,similar to findOneandUpdate
 
-            // updateMany:
+    //         // updateMany:
 
-            return{
-                status:true,
-                Data: updatedUser
-            }
-        } catch(error) {
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while updating user'
-            }
-        }
-    }
+    //         return{
+    //             status:true,
+    //             Data: updatedUser
+    //         }
+    //     } catch(error) {
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while updating user'
+    //         }
+    //     }
+    // }
 
-    async insertUserData(userData) {
-        try {
-            let userDataInserted = await this.userDataModel.create(userData)
-            return {
-                status: true,
-                data: userDataInserted
-            }
-        }
-        catch(error) {
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while inserting user'
-            }
-        }
-    }
+    // async insertUserData(userData) {
+    //     try {
+    //         let userDataInserted = await this.userDataModel.create(userData)
+    //         return {
+    //             status: true,
+    //             data: userDataInserted
+    //         }
+    //     }
+    //     catch(error) {
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while inserting user'
+    //         }
+    //     }
+    // }
 
-    async collectUserDataOfUser(userId) {
-        try {
-            let allDataOfUser = await this.userModel.aggregate([
-                {
-                    $match: {_id: userId}
-                },
-                {
-                    $lookup: {
-                        from: 'user-data',
-                        localField: '_id',
-                        foreignField: 'user_id',
-                        as: 'userData'
-                    },
+    // async collectUserDataOfUser(userId) {
+    //     try {
+    //         let allDataOfUser = await this.userModel.aggregate([
+    //             {
+    //                 $match: {_id: userId}
+    //             },
+    //             {
+    //                 $lookup: {
+    //                     from: 'user-data',
+    //                     localField: '_id',
+    //                     foreignField: 'user_id',
+    //                     as: 'userData'
+    //                 },
                     
-                    $project: {
-                        name: 1,
-                        email: 1,
-                        mobile_number: 1,
-                        age: 1,
-                        id_card: '$userDatas.id_card',
-                        roll_number: '$userDatas.roll_number'
-                    }
-                }
-            ])
+    //                 $project: {
+    //                     name: 1,
+    //                     email: 1,
+    //                     mobile_number: 1,
+    //                     age: 1,
+    //                     id_card: '$userDatas.id_card',
+    //                     roll_number: '$userDatas.roll_number'
+    //                 }
+    //             }
+    //         ])
 
-            return {
-                status: true,
-                data: allDataOfUser
-            }
-        }
-        catch {
-            console.log(error);
-            return {
-                status: false,
-                message: 'Error in services while collecting all data of user'
-            }
-        }
-    }
+    //         return {
+    //             status: true,
+    //             data: allDataOfUser
+    //         }
+    //     }
+    //     catch {
+    //         console.log(error);
+    //         return {
+    //             status: false,
+    //             message: 'Error in services while collecting all data of user'
+    //         }
+    //     }
+    // }
 }
 
 module.exports = new UserService();
